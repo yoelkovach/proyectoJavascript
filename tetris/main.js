513206275edb65c6f9201c7fc305bd7a3cffda3f
yoelkovach <yoelkovach@outlook.es>
2024-10-17 22:59:37 +0200

    movent piace

--- tetris/main.js
+++ tetris/main.js
@@ -45,16 +45,23 @@ const board =[
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1]
 ]
 
+// 4 piace player
+const piace = {
+    position: {x: 5, y: 5},
+    shape: [
+        [1,1],
+        [1,1]
+    ]
+}
 
 // 2. game loop
 
-function updtade(){
+function update(){
     draw()
-    window.requestAnimationFrame(updtade)
+    window.requestAnimationFrame(update)
 
 }
 
@@ -62,15 +69,86 @@ function draw() {
     $context.fillStyle = '#000'
     $context.fillRect(0, 0, $canvas.width, $canvas.height)
 
-    board.forEach((row, y)=>{
-        row.forEach((value, x)=>{
+    board.forEach((row, y) => {
+        row.forEach((value, x) => {
             if (value === 1) {
                 $context.fillStyle = 'yellow'
                 $context.fillRect(x, y, 1, 1)
             }
         })
     })
+
+    piace.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value){
+                $context.fillStyle = 'red'
+                $context.fillRect(x + piace.position.x, y + piace.position.y, 1, 1)
+            }
+        })
+    })
 }
 
-updtade()
+document.addEventListener('keydown', event => {
+    
+    if (event.key === 'ArrowLeft'){
+        piace.position.x--
+        if (checkCollision()) {
+            piace.position.x++
+        }
+    }
+    if (event.key === 'ArrowRight'){
+        piace.position.x++
+        if (checkCollision()) {
+            piace.position.x--
+        }
+    }
+    if (event.key === 'ArrowDown'){
+        piace.position.y++
+        if (checkCollision()) {
+            piace.position.y--
+            solidfyPiace()
+            removeRows()
+        }
+    }
+})
+
+function checkCollision() {
+    return piace.shape.find((row, y) => {
+        return row.find((value, x) => {
+            return (
+                value != 0 && board[y + piace.position.y]?.[x + piace.position.x] != 0
+            )
+        })
+    })
+}
+
+function solidfyPiace() {
+    piace.shape.forEach((row, x) => {
+        row.forEach((value, y) => {
+            if (value === 1 ){
+                board[y + piace.position.y][x + piace.position.x] = 1
+            }
+        })
+    })
+
+    piace.position.x = 0
+    piace.position.y = 0
+}
+
+function removeRows() {
+    const rowsToRemove = []
+
+    board.forEach((row, y) => {
+        if (row.every(value => value === 1)) {
+            rowsToRemove.push(y)
+        }
+    })
+
+    rowsToRemove.forEach(y => {
+        board.splice(y, 1)
+        const newRow = Array(BOARD_WIDTH).fill(0)
+        board.unshift(newRow)
+    })
+}
+update()
 
